4272b6afd0cb0d16601e4f1a54f434a60fd790c3
Igor Grešovnik <gresovnik@gmail.com>
2025-03-07 00:27:08 +0100

    Added update / clone scripts for web pages repos

---
+++ UpdateReposGroup_All.ps1
@@ -0,0 +1,26 @@
+
+# Clones or updates all repositories in this container repository.
+Write-Host "`n`nCloning / updating all repositories in this container ...`n"
+
+# Get the script directory such that relative paths can be resolved:
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDir = Split-Path $scriptPath -Parent
+$scriptFilename = [System.IO.Path]::GetFileName($scriptPath)
+
+Write-Host "Script directory: $scriptDir"
+
+Write-Host "`nUpdating :"
+& $(Join-Path $scriptDir "UpdateRepo_IGLibFramework.ps1")
+
+Write-Host "`nUpdating :"
+& $(Join-Path $scriptDir "UpdateRepo_igor.ps1")
+
+Write-Host "`nUpdating :"
+& $(Join-Path $scriptDir "UpdateRepo_IGLibFrameworkCodedoc.ps1")
+
+Write-Host "`nUpdating Inverse:"
+& $(Join-Path $scriptDir "UpdateRepo_Inverse.ps1")
+
+
+Write-Host "  ... updating all repositoris in this container completed.`n`n"
+
